06baf4ef3800dcacf04dd1d5ad2cdc2844cf880e
kittysmith5 <7880566+kittysmith5@user.noreply.gitee.com>
2022-05-24 15:02:03 +0800

    update the bug for unban member

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 ## How to use
 ```shell
 
+git clone https://github.com/kittysmith5/grp_verification_bot_v5.git
+
+cd grp_verification_bot_v5
+
 go build -o bot .
 
 ./bot -t YOU_TELEGRAM_BOT_TOKEN
--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "v5tgbot/msg_handler"
+	"flag"
+	"fmt"
+	"v5tgbot/msg_handler"
 )
 
-var testMap = map[int]int{}
-
 func main() {
-    //token := flag.String("t", "", "token of your bot")
-    token := flag.String("t", "YOU_TELEGRAM_BOT_TOKEN", "token of your bot")
-    flag.Parse()
-    fmt.Println(*token)
-    msg_handler.Boot(*token)
+	//token := flag.String("t", "", "token of your bot")
+	token := flag.String("t", "", "token of your bot")
+	flag.Parse()
+	fmt.Println(*token)
+	msg_handler.Boot(*token)
 }
